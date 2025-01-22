6c6d8e7f565ccc3f23d34932c7142d520adc306b
Germán Carrillo <gcarrillo@linuxmail.org>
2025-01-22 17:11:00 +0100

    [tests] Initial test infrastructure with sample test

---
+++ tests/test_provider_load.py
@@ -0,0 +1,29 @@
+import nose2
+
+from qgis.core import QgsApplication
+from qgis.testing import unittest, start_app
+from pzp.processing.provider import Provider
+
+start_app()
+
+
+class TestProviderLoad(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('\nINFO: Set up test_provider_load')
+        cls.provider = Provider()  # Processing provider
+        QgsApplication.processingRegistry().addProvider(cls.provider)
+
+    def test_provider_load(self):
+        print('INFO: Validating provider load...')
+        self.assertIn("PZP_UTILS", [provider.name() for provider in QgsApplication.processingRegistry().providers()])
+
+    @classmethod
+    def tearDownClass(cls):
+        print('INFO: Tear down test_provider_load')
+        QgsApplication.processingRegistry().removeProvider(cls.provider)
+
+
+if __name__ == '__main__':
+    nose2.main()
