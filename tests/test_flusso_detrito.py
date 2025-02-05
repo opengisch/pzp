be422be318bb9ee8767f2f1901e0ebebe55f2320
Germán Carrillo <gcarrillo@linuxmail.org>
2025-02-05 15:39:46 +0100

    [tests] Flusso detrito: test geometries, attributes, counts per category and post layer configurations

--- tests/test_flusso_detrito.py
+++ tests/test_flusso_detrito.py
@@ -1,12 +1,13 @@
 import pytest
-from qgis.core import QgsMapLayer, QgsProject, QgsVectorLayer
+from qgis.core import QgsExpressionContextUtils, QgsMapLayer, QgsProject, QgsVectorLayer
 from qgis.testing import start_app
 from qgis.testing.mocked import get_iface
 
 from pzp.calculation import CalculationDialog
-from tests.utils import get_copy_path
+from tests.utils import get_copy_path, get_data_path, get_expected_path
 
 start_app()
+import processing
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -21,7 +22,19 @@ def initialize_processing():
 def flusso_detrito_layer():
     print("\nINFO: Get layer copy")
     yield QgsVectorLayer(
-        str(get_copy_path("riali_gambarogno_1200.gpkg")) + "|layername=Intensità completa", "layer", "ogr"
+        str(get_copy_path(get_data_path("riali_gambarogno_1200.gpkg"))) + "|layername=Intensità completa",
+        "layer",
+        "ogr",
+    )
+
+
+@pytest.fixture(scope="module")
+def flusso_detrito_expected_layer():
+    print("\nINFO: Get read-only expected layer")
+    yield QgsVectorLayer(
+        str(get_expected_path("riali_gambarogno_1200.gpkg")) + "|layername=Pericolo 1200 20250204170251",
+        "expected layer",
+        "ogr",
     )
 
 
@@ -38,11 +51,15 @@ def plugin_instance():
 
 
 @pytest.mark.flusso_detrito
-def test_flusso_detrito(plugin_instance, flusso_detrito_layer):
+def test_flusso_detrito(plugin_instance, flusso_detrito_layer, flusso_detrito_expected_layer):
     print(" [INFO] Validating flusso detrito...")
     process_type = 1200
 
+    # Make sure we have valid input/expected data layers
     assert flusso_detrito_layer.isValid()
+    assert flusso_detrito_layer.featureCount() == 268
+    assert flusso_detrito_expected_layer.isValid()
+    assert flusso_detrito_expected_layer.featureCount() == 101
 
     # Add layer to project so that Processing can find it and use it
     QgsProject.instance().addMapLayer(flusso_detrito_layer)
@@ -53,6 +70,67 @@ def test_flusso_detrito(plugin_instance, flusso_detrito_layer):
     assert isinstance(pericolo_layer, QgsMapLayer)
     assert pericolo_layer.featureCount() == 101
 
-    # TODO: Compare features per category
+    # Check post layer configurations
+    options = pericolo_layer.geometryOptions()
+    assert options.geometryPrecision() == 0.001
+    assert options.removeDuplicateNodes()
+    assert options.geometryChecks() == ["QgsIsValidCheck"]
 
-    # TODO: Compare geometries
+    assert QgsExpressionContextUtils.layerScope(pericolo_layer).variable("pzp_layer") == "danger_zones"
+    assert QgsExpressionContextUtils.layerScope(pericolo_layer).variable("pzp_process") == "1200"
+
+    # Compare features per category
+    statistics = processing.run(
+        "qgis:statisticsbycategories",
+        {
+            "INPUT": pericolo_layer,
+            "VALUES_FIELD_NAME": "",
+            "CATEGORIES_FIELD_NAME": ["grado_pericolo"],
+            "OUTPUT": "TEMPORARY_OUTPUT",
+        },
+    )
+    statistics_layer = statistics["OUTPUT"]
+    assert isinstance(statistics_layer, QgsMapLayer)
+    assert statistics_layer.isValid()
+
+    expected_features_per_group = {
+        1000: 19,  # non in pericolo
+        1001: 8,  # residuo
+        1002: 16,  # basso
+        1003: 30,  # medio
+        1004: 28,  # elevato
+    }
+    assert statistics_layer.featureCount() == 5
+    for feature in statistics_layer.getFeatures():
+        assert expected_features_per_group.get(feature["grado_pericolo"], -1) == feature["count"]
+
+    # Compare geometries and attributes (expected layer vs obtained layer)
+    layer_comparison = processing.run(
+        "native:detectvectorchanges",
+        {
+            "ORIGINAL": flusso_detrito_expected_layer,
+            "REVISED": pericolo_layer,
+            "COMPARE_ATTRIBUTES": [  # To test only geometries, pass empty list here
+                # 'fid',
+                "commento",
+                "periodo_ritorno",
+                "classe_intensita",
+                "proc_parz",
+                "fonte_proc",
+                "grado_pericolo",
+                "matrice",
+                # 'layer',
+            ],
+            "MATCH_TYPE": 1,  # 0: Exact match, 1: Tolerant match
+            "UNCHANGED": "TEMPORARY_OUTPUT",
+            "ADDED": "TEMPORARY_OUTPUT",
+            "DELETED": "TEMPORARY_OUTPUT",
+        },
+    )
+    assert isinstance(layer_comparison["UNCHANGED"], QgsMapLayer)
+    assert isinstance(layer_comparison["ADDED"], QgsMapLayer)
+    assert isinstance(layer_comparison["DELETED"], QgsMapLayer)
+
+    assert layer_comparison["UNCHANGED"].featureCount() == 101  # THe obtained layer matches the expected one
+    assert layer_comparison["ADDED"].featureCount() == 0
+    assert layer_comparison["DELETED"].featureCount() == 0
--- tests/utils.py
+++ tests/utils.py
@@ -2,14 +2,31 @@ import shutil
 from datetime import datetime
 from pathlib import Path
 from tempfile import gettempdir
+from typing import Optional
 
 
-def get_copy_path(file_name: str) -> Path:
-    data_dir = Path.cwd() / "tests" / "data"
-    src_path = data_dir / file_name
+def get_data_path(file_name: Optional[str] = "") -> Path:
+    """
+    Returns the path of the "data" dir for tests, or, if file_name is
+    given, returns the file_name appended to the data dir for tests.
+    """
+    return Path.cwd() / "tests" / "data" / file_name if file_name else Path.cwd() / "tests" / "data"
 
-    dst_path = get_tmp_path(file_name)
-    shutil.copyfile(str(src_path), str(dst_path))
+
+def get_expected_path(file_name: Optional[str] = "") -> Path:
+    """
+    Returns the path of the "expected" dir for tests, or, if file_name is
+    given, returns the file_name appended to the expected dir for tests.
+    """
+    return Path.cwd() / "tests" / "expected" / file_name if file_name else Path.cwd() / "tests" / "expected"
+
+
+def get_copy_path(file_path: Path) -> Path:
+    """
+    Returns a path to a copied version of the file. The file should exist.
+    """
+    dst_path = get_tmp_path(file_path.name)
+    shutil.copyfile(str(file_path), str(dst_path))
     return dst_path
 
 
