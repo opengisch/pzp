15a18a3338230ac5032e719a511c97465727aaef
Damiano Lombardi <damiano@opengis.ch>
2025-04-03 18:03:04 +0200

    workaround for qgis 3.28

--- pzp/utils/settings.py
+++ pzp/utils/settings.py
@@ -1,4 +1,11 @@
-from qgis.core import QgsSettingsEntryDouble, QgsSettingsTree
+from qgis.core import QgsSettingsEntryDouble
+
+# Remove this when dropping QGIS 3.28 compatibility
+qgsSettingsTreeAvailable = True
+try:
+    from qgis.core import QgsSettingsTree
+except ImportError:
+    qgsSettingsTreeAvailable = False
 
 PLUGIN_NAME = "PZP"
 
@@ -10,12 +17,17 @@ class Settings:
         if cls.instance is None:
             cls.instance = super(Settings, cls).__new__(cls)
 
-            settings_node = QgsSettingsTree.createPluginTreeNode(pluginName=PLUGIN_NAME)
-
-            cls.merge_form_factor = QgsSettingsEntryDouble("merge_form_factor", settings_node, 0.0)
+            if qgsSettingsTreeAvailable:
+                settings_node = QgsSettingsTree.createPluginTreeNode(pluginName=PLUGIN_NAME)
+                cls.merge_form_factor = QgsSettingsEntryDouble("merge_form_factor", settings_node, 0.0)
+            else:
+                cls.merge_form_factor = QgsSettingsEntryDouble(f"plugins/{PLUGIN_NAME}/merge_form_factor", None, 0.0)
 
         return cls.instance
 
     @staticmethod
     def unload():
+        if not qgsSettingsTreeAvailable:
+            return
+
         QgsSettingsTree.unregisterPluginTreeNode(PLUGIN_NAME)
